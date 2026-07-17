154652b68a2da82878da663fd10269148face6b9
Appropriate32 <frostghost32@gmail.com>
2026-07-17 23:50:39 +0500

    Added constructor, some properties, hit and isSunk method to Ship

--- src/Ship.js
+++ src/Ship.js
@@ -1,3 +1,19 @@
-class Ship {}
+class Ship {
+  constructor() {
+    this.length = 0;
+    this.numberOfHits = 0;
+    this.hasSunk = false;
+  }
+
+  hit() {
+    this.numberOfHits++;
+  }
+
+  isSunk() {
+    if (this.numberOfHits >= this.length) {
+      this.hasSunk = true;
+    }
+  }
+}
 
 export default Ship;
