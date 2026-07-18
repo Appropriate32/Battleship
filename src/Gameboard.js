279adb822d4d9c7eea7c64f700cb9fcb361d2809
Appropriate32 <frostghost32@gmail.com>
2026-07-18 21:24:32 +0500

    tested and implemented placeShip method, started testing on receiveAttack method

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -5,11 +5,43 @@ class Gameboard {
     this.grid = Array.from({ length: 10 }, () => Array(10).fill(null));
   }
 
-  placeShip(x, y, length) {
+  placeShip(x, y, length, direction) {
     const shipObj = new Ship(length);
-    for (let i = 0; i < length; i++) {
-      this.grid[x++][y] = shipObj;
+    if (direction === "vertical") {
+      if (y + (length - 1) > 9) {
+        for (let i = 0; i < length; i++) {
+          this.grid[x][y--] = shipObj;
+        }
+      } else {
+        for (let i = 0; i < length; i++) {
+          this.grid[x][y++] = shipObj;
+        }
+      }
+
+      return;
     }
+
+    if (direction === "horizontal") {
+      if (x + (length - 1) > 9) {
+        for (let i = 0; i < length; i++) {
+          this.grid[x--][y] = shipObj;
+        }
+      } else {
+        for (let i = 0; i < length; i++) {
+          this.grid[x++][y] = shipObj;
+        }
+      }
+      return;
+    }
+  }
+
+  receiveAttack(x, y) {
+    if (this.grid[x][y] !== null) {
+      this.grid[x][y].hit();
+      return;
+    }
+
+    return [x, y];
   }
 }
 
--- src/Gameboard.test.js
+++ src/Gameboard.test.js
@@ -7,39 +7,121 @@ test("By default, there should be null ship at coordinates", () => {
   expect(board.grid[0][0]).toBe(null);
 });
 
-test("There should be a ship present at 0,0 coordinates", () => {
-  const board = new Gameboard();
+describe("placeShip method", () => {
+  let board;
 
-  board.placeShip(0, 0, 1);
+  beforeEach(() => {
+    board = new Gameboard();
+  });
 
-  expect(board.grid[0][0]).toBeInstanceOf(Ship);
+  test("There should be a ship present at 0,0 coordinates", () => {
+    board.placeShip(0, 0, 1, "horizontal");
+
+    expect(board.grid[0][0]).toBeInstanceOf(Ship);
+  });
+
+  test("There should be a ship present at 3,2 coordinates", () => {
+    board.placeShip(3, 2, 1, "horizontal");
+
+    expect(board.grid[3][2]).toBeInstanceOf(Ship);
+  });
+
+  test("There should be a ship present of length 3 at 0,0 coordinates that takes up 1,0 and 2,0 aswell ", () => {
+    board.placeShip(0, 0, 3, "horizontal");
+
+    expect(board.grid[0][0]).toBeInstanceOf(Ship);
+    expect(board.grid[1][0]).toBeInstanceOf(Ship);
+    expect(board.grid[2][0]).toBeInstanceOf(Ship);
+  });
+
+  test("There should be a ship present of length 3 at 0,0 coordinates that takes up 1,0 and 2,0 aswell, but it should not take up 3,0 ", () => {
+    board.placeShip(0, 0, 3, "horizontal");
+
+    expect(board.grid[0][0]).toBeInstanceOf(Ship);
+    expect(board.grid[1][0]).toBeInstanceOf(Ship);
+    expect(board.grid[2][0]).toBeInstanceOf(Ship);
+    expect(board.grid[3][0]).toBeNull();
+  });
+
+  test("There should be a ship present of length 3 at 0,0 coordinates that takes up 0,1 and 0,2 aswell ", () => {
+    board.placeShip(0, 0, 3, "vertical");
+
+    expect(board.grid[0][0]).toBeInstanceOf(Ship);
+    expect(board.grid[0][1]).toBeInstanceOf(Ship);
+    expect(board.grid[0][2]).toBeInstanceOf(Ship);
+  });
+
+  test("There should be a ship present of length 3 at 0,0 coordinates that takes up 0,1 and 0,2 aswell, but not at 0,3 ", () => {
+    board.placeShip(0, 0, 3, "vertical");
+
+    expect(board.grid[0][0]).toBeInstanceOf(Ship);
+    expect(board.grid[0][1]).toBeInstanceOf(Ship);
+    expect(board.grid[0][2]).toBeInstanceOf(Ship);
+    expect(board.grid[0][3]).toBeNull();
+  });
+
+  test("There should be a ship present of length 3 at 9,0 coordinates that takes up 8,0 and 7,0 aswell", () => {
+    board.placeShip(9, 0, 3, "horizontal");
+
+    expect(board.grid[9][0]).toBeInstanceOf(Ship);
+    expect(board.grid[8][0]).toBeInstanceOf(Ship);
+    expect(board.grid[7][0]).toBeInstanceOf(Ship);
+  });
+
+  test("There should be a ship present of length 3 at 8,0 coordinates that takes up 7,0 and 6,0 aswell", () => {
+    board.placeShip(8, 0, 3, "horizontal");
+
+    expect(board.grid[8][0]).toBeInstanceOf(Ship);
+    expect(board.grid[7][0]).toBeInstanceOf(Ship);
+    expect(board.grid[6][0]).toBeInstanceOf(Ship);
+  });
+
+  test("There should be a ship present of length 3 at 0,9 coordinates that takes up 0,8 and 0,7 aswell", () => {
+    board.placeShip(0, 9, 3, "vertical");
+
+    expect(board.grid[0][9]).toBeInstanceOf(Ship);
+    expect(board.grid[0][8]).toBeInstanceOf(Ship);
+    expect(board.grid[0][7]).toBeInstanceOf(Ship);
+  });
+
+  test("There should be a ship present of length 3 at 0,8 coordinates that takes up 0,7 and 0,6 aswell", () => {
+    board.placeShip(0, 8, 3, "vertical");
+
+    expect(board.grid[0][8]).toBeInstanceOf(Ship);
+    expect(board.grid[0][7]).toBeInstanceOf(Ship);
+    expect(board.grid[0][6]).toBeInstanceOf(Ship);
+  });
 });
 
-test("There should be a ship present at 3,2 coordinates", () => {
-  const board = new Gameboard();
+describe("receiveAttack method", () => {
+  let board;
 
-  board.placeShip(3, 2, 1);
+  beforeEach(() => {
+    board = new Gameboard();
+    board.placeShip(0, 0, 3, "horizontal");
+  });
 
-  expect(board.grid[3][2]).toBeInstanceOf(Ship);
-});
+  test("ship should be hit and its number of hits should increase by 1", () => {
+    board.receiveAttack(0, 0);
 
-test("There should be a ship present of length 3 at 0,0 coordinates that takes up 1,0 and 2,0 aswell ", () => {
-  const board = new Gameboard();
+    expect(board.grid[0][0].numberOfHits).toBe(1);
+  });
 
-  board.placeShip(0, 0, 3);
+  test("ship should be hit twice and its number of hits should increase by 2", () => {
+    board.receiveAttack(0, 0);
+    board.receiveAttack(0, 0);
 
-  expect(board.grid[0][0]).toBeInstanceOf(Ship);
-  expect(board.grid[1][0]).toBeInstanceOf(Ship);
-  expect(board.grid[2][0]).toBeInstanceOf(Ship);
-});
+    expect(board.grid[0][0].numberOfHits).toBe(2);
+  });
 
-test("There should be a ship present of length 3 at 0,0 coordinates that takes up 1,0 and 2,0 aswell, but it should not take up 3,0 ", () => {
-  const board = new Gameboard();
+  test("same ship should be hit twice in two different coordinates and its number of hits should increase by 2", () => {
+    board.receiveAttack(0, 0);
+    board.receiveAttack(1, 0);
 
-  board.placeShip(0, 0, 3);
+    expect(board.grid[0][0].numberOfHits).toBe(2);
+  });
 
-  expect(board.grid[0][0]).toBeInstanceOf(Ship);
-  expect(board.grid[1][0]).toBeInstanceOf(Ship);
-  expect(board.grid[2][0]).toBeInstanceOf(Ship);
-  expect(board.grid[3][0]).toBeNull();
+  test("attack should miss on 5, 4 and missed coordinates returned 5, 4", () => {
+    expect(board.receiveAttack(5, 4)).toEqual([5, 4]);
+  });
 });
