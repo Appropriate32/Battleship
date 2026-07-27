6325215510d893aeec6e4670688619c8ee89f167
Appropriate32 <frostghost32@gmail.com>
2026-07-27 20:31:42 +0500

    Implemented the visual layout

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import "./styles.css";
 import "nes.css/css/nes.min.css";
+
+const boardOne = document.querySelector(".board-one");
+const boardTwo = document.querySelector(".board-two");
+
+for (let i = 0; i < 100; i++) {
+  const cell = document.createElement("div");
+  cell.classList.add("nes-pointer");
+  cell.classList.add("board-cell");
+  boardOne.appendChild(cell);
+}
+
+for (let i = 0; i < 100; i++) {
+  const cell = document.createElement("div");
+  cell.classList.add("nes-pointer");
+  cell.classList.add("board-cell");
+  boardTwo.appendChild(cell);
+}
